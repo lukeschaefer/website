2c84a4a25ae697ea1c81a07c26d40625745be935
Luke Schaefer <5386710+lukeschaefer@users.noreply.github.com>
2023-11-03 19:04:04 +0000

    Cleans up styles on mobile

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'LukeSchaefer.Dev';
+export const SITE_TITLE = 'Luke';
 export const SITE_DESCRIPTION = 'Personal blog and website of Luke Schaefer';
